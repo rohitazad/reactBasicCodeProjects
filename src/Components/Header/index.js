6641c9db1822f3cab726f442685881aa675b7af1
rohitazad <ram9953878727@gmail.com>
2022-10-28 12:48:06 +0530

    third party lib add in react js throw npm

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 const HeaderComponents = ()=>{
     return (
         <header>
-            hello  i m header
+           <Navbar bg="primary" variant="dark">
+            <Container>
+             <Navbar.Brand href="#home">Navbar React</Navbar.Brand>
+             <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+            </Container>
+           </Navbar>
         </header>
     )
 }
--- src/Container/Home/index.js
+++ src/Container/Home/index.js
@@ -2,6 +2,14 @@ import React, { useState } from 'react';
 import H2TagComponents from '../../Components/H2Tag';
 import HeaderComponents from '../../Components/Header';
 import FotterComponents from '../../Components/Footer';
+
+import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 const HomeContainer = ()=>{
     const [number, setNumber] = useState(5);
     const h2tagArrayData = ['Rohit', 'Abhishek'];
@@ -16,15 +24,35 @@ const HomeContainer = ()=>{
     return (
         <>
         <HeaderComponents/>
-            {
-                h2tagArrayData && h2tagArrayData.length > 0 ? handleData() : 'Not data  found'
-            }
-            <br />
-            <hr/>
-            <p>My updated value is :- {number}</p>
-            <button onClick={()=>{setNumber(number + 1)}}>+ increase</button>
-            <button onClick={()=>{setNumber(number - 1)}}>- decrease</button>
-            <button onClick={()=>{setNumber(5)}}>Reset</button>
+        <Container>
+            <Row>
+                <Col md={6} xs={12}>
+                    {
+                        h2tagArrayData && h2tagArrayData.length > 0 ? handleData() : 'Not data  found'
+                    }
+                    <br />
+                    <hr/>
+                    <p>My updated value is :- 
+                        <Badge bg="secondary">
+                            {number}
+                        </Badge>
+                    </p>
+                </Col>
+                <Col md={6} xs={12} className="rightSecWrap">
+                    <Button onClick={()=>{setNumber(number + 1)}} variant="warning">  Increase value
+                            <Badge bg="secondary">
+                                +
+                            </Badge> 
+                    </Button>
+                    <Button variant="danger" onClick={()=>{setNumber(number - 1)}}>
+                            -
+                    </Button>
+                    <button onClick={()=>{setNumber(5)}}>Reset</button>
+                </Col>
+            </Row>
+        </Container>
+
+        
         <FotterComponents />
         </>
     )
